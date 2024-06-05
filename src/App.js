63e19a3700a4aefe28468980a64f3eb1770145b4
Dean Papas <deanpapas@bigpond.com>
2024-06-05 17:25:48 +1000

    Place Holder Info

--- src/App.js
+++ src/App.js
@@ -1,65 +1,30 @@
 import "./App.css";
-import NavMenu from "./components/navmenu/NavMenu";
-
-import React, { useEffect, useState } from "react";
 import {
   LinkedinOutlined,
   GithubOutlined,
+  CaretRightOutlined,
   MailOutlined,
 } from "@ant-design/icons";
 import Section from "./components/section/Section";
 
 function App() {
-  const [currentSections, setCurrentSections] = useState(new Set());
-  const sectionTitles = document.querySelectorAll(".section-title");
-
-  useEffect(() => {
-    const scrollElement = document.getElementById("scrollID");
-    if (scrollElement) {
-      scrollElement.addEventListener("scroll", handleScroll);
-    }
-
-    return () => {
-      if (scrollElement) {
-        scrollElement.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
-
-  const handleScroll = () => {
-    for (let i = 0; i < sectionTitles.length; i++) {
-      if (isElementOnScreen(sectionTitles[i])) {
-        currentSections.add(sectionTitles[i].textContent);
-      } else {
-        currentSections.delete(sectionTitles[i].textContent);
-      }
-    }
-    console.log(currentSections);
-  };
-
-  const isElementOnScreen = (elem) => {
-    const rect = elem.getBoundingClientRect();
-    return rect.top >= 0 && rect.bottom <= window.innerHeight;
-  };
-
   return (
     <body className="body" id="scrollID">
-      <header className="header">
-        <div className="nav">
-          {/* <NavMenu currentSections={currentSections} /> */}
-        </div>
-      </header>
 
       <div className="side-display-left">
         <div className="vertical-line-top"></div>
         <div className="side-display-left-icon">
-          <LinkedinOutlined />
+          <a
+            href="https://www.linkedin.com/in/dean-papadofrangakis-22b166247/"
+            target="_blank"
+          >
+            <LinkedinOutlined />
+          </a>
         </div>
         <div className="side-display-left-icon">
-          <GithubOutlined />
-        </div>
-        <div className="side-display-left-icon">
-          <MailOutlined />
+          <a href="https://github.com/deanpapas" target="_blank">
+            <GithubOutlined />
+          </a>
         </div>
         <div className="vertical-line-bottom"></div>
       </div>
@@ -71,7 +36,7 @@ function App() {
       </div>
 
       <div className="content">
-        <div className="section" id="introduction">
+        <div className="section">
           <h1>G'day, I'm</h1>
           <h2>Dean Papas.</h2>
           <h3>Software Engineering Student</h3>
@@ -81,10 +46,76 @@ function App() {
           </p>
         </div>
 
-        <Section title="About Me" children="" />
-        <Section title="Experience" children="" />
-        <Section title="Projects" children="" />
-        <Section title="Contact" children="" />
+        <Section title="About Me">
+          <div className="section">
+            <p>
+              Hello! My name is Dean, a Software Engineering student at RMIT
+              University in Melbourne, Australia. My passion for software
+              development began when I was in high school, starting with simple
+              HTML and CSS websites. Since then, I have expanded my knowledge to
+              include a variety of programming languages and frameworks,
+              including Python, Java, Delphi and React.
+            </p>
+            <p>
+              Outside of programming, I enjoy building computers, rock climbing
+              and baking.
+            </p>
+          </div>
+        </Section>
+        <Section title="Experience" children="">
+          <div className="section">
+            <h3>
+              IntegraDev | 2023 - Present
+            </h3>
+            <p>
+              Currently I am working as a Software Developer at{" "}
+              <a id="hyperlink" href="https://www.integradev.com.au/">
+                IntegraDev
+              </a>
+              , a software company developing financial planning tools for the
+              financial services industry.
+            </p>
+            <p>
+              {" "}
+              <CaretRightOutlined />
+              Object Pascal{" "}
+            </p>
+            <p>
+              {" "}
+              <CaretRightOutlined />
+              SQL{" "}
+            </p>
+            <p>
+              {" "}
+              <CaretRightOutlined />
+              Git{" "}
+            </p>
+            <p>
+              {" "}
+              <CaretRightOutlined />
+              GitHub{" "}
+            </p>
+          </div>
+        </Section>
+
+        {/* <Section title="Projects" children="" /> */}
+        <Section title="Contact" children="">
+          <div className="section">
+            <p>
+              Even though I'm not currently looking for new opportunities, my
+              inbox is always open. Whether for a potential project or just to
+              say hi, I'll try my best to answer your email!
+            </p>
+            <p>
+              <div id="contact-icons">
+                {" "}
+                <a id="contact-icon" href="mailto:contact@deanpapas.dev">
+                  <MailOutlined />
+                </a>
+              </div>
+            </p>
+          </div>
+        </Section>
       </div>
     </body>
   );
